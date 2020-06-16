b40b83cd7d025f103b68cd7a7bc3bcf138562013
Andreas Zoellner <andreas.zoellner@initos.com>
2020-06-16 13:14:26 +0200

    [FIX] Enable kwarg 'encoding' for 'open()' for Python2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from os import path
 from setuptools import setup
 
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+with io.open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 about = {}
